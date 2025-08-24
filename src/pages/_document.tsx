e404ff4842a4d6d689cef0387f5b9c18e8fac82b
Zhakhangir Anuarbek <zhaxa65@gmail.com>
2025-08-24 14:17:28 +0500

    refactor: Add Yandex.Metrika tracking script and noscript fallback to Document component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,6 +17,33 @@ export default function Document() {
         <meta property="og:url" content="https://cross.sport.mos.ru/" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/cross_preview.jpg" />
+
+        {/* Yandex.Metrika counter */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103870555', 'ym');
+
+              ym(103870555, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/103870555"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </Head>
       <body className="antialiased">
         <div
